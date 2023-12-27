57274069610fb4b4e2f07f0c51952c3df208ea5b
abdo <aboabdo8600@gmail.com>
2023-12-27 21:04:25 +0200

    firsta

--- src/components/1-Header/Header.jsx
+++ src/components/1-Header/Header.jsx
@@ -7,22 +7,12 @@ const Header = () => {
   return (
     <header className='d-flex justify-content-between  container  my-3 align-items-center'>
         <img src={Logo} alt="Logo" style={{width:"150px" , height:"35px"}} className='me-3 img' />
-<div className='d-flex align-items-center  mail  icon-email'>
-            {/* <img src={Mail} alt="Mail"  style={{width:"25px" ,}}  /> */}
-            <div className='mx-2  '>
-                <p style={{lineHeight:"10px"  ,color:"#093266" }}>Mail Us On</p>
-                <p style={{lineHeight:"2px" , color:"#f7921e"}}>Info@goldenPath</p>
-            </div>
-    
-</div>        
-<div className='d-flex  align-items-center'>
-<div className='icon-phone' />
-        {/* <img src={Phone} alt="Phone"  style={{width:"25px"}} /> */}
+       
+
         <div className='mx-2 mail '>
-                <p style={{lineHeight:"10px" ,color:"#093266" }}>Make a call</p>
-                <p style={{lineHeight:"2px"  , color:"#f7921e"}}>+123456789</p>
+                <p className='icon-email mb-4' style={{lineHeight:"10px" ,color:"#093266" }}> Info@goldenPath</p>
+                <p className='icon-phone ' style={{lineHeight:"2px"  , color:"#093266"}}> +123456789</p>
             </div>
-        </div>        
 
     </header>
   )
