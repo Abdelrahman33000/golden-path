f5f3ac3d3faae962bb95b23af66d5b3f699664a1
abdo <aboabdo8600@gmail.com>
2023-12-27 21:15:20 +0200

    first

--- src/components/1-Header/Header.jsx
+++ src/components/1-Header/Header.jsx
@@ -5,11 +5,11 @@ import Logo from '../images/Golden Path - Logo.png'
 import './Header.css'
 const Header = () => {
   return (
-    <header className='d-flex justify-content-between  container  my-3 align-items-center'>
+    <header className='d-flex justify-content-around    my-3 align-items-center'>
         <img src={Logo} alt="Logo" style={{width:"150px" , height:"35px"}} className='me-3 img' />
        
 
-        <div className='ms-2 mail '>
+        <div className=' mail '>
                 <p className='icon-email mb-4' style={{lineHeight:"10px" ,color:"#093266" }}> Info@goldenPath</p>
                 <p className='icon-phone ' style={{lineHeight:"2px"  , color:"#093266"}}> +123456789</p>
             </div>
