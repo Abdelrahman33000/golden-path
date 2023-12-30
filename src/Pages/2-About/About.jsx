9e8e86afdde678b36020f9605c266c60a5933d14
abdo <aboabdo8600@gmail.com>
2023-12-30 12:55:04 +0200

    firstl

--- src/Pages/2-About/About.jsx
+++ src/Pages/2-About/About.jsx
@@ -1,9 +1,189 @@
 import React from 'react'
+import CountUp from 'react-countup';
+
+import Carousel from 'react-bootstrap/Carousel';
+import img   from '../../components/images/cameraabout.png';
+import img2   from '../../components/images/slide1.jpg';
+import img3   from '../../components/images/Scamera.jpg';
+import { Balance, Beenhere, CameraRoll, Cottage, Engineering, LaptopChromebook, LocalPhone, TaskAlt} from '@mui/icons-material'
+import './About.css'
 
 const About = () => {
   return (
-    <div style={{display:"flex" , justifyContent:"center" , alignItems:"center" , minHeight:"100vh"}}>About</div>
-  )
+ <div>
+     <Carousel fade className='Carousel'>
+     <Carousel.Item>
+       <img  src={img } alt='' text="First slide"  style={{width:"100%" , height:"450px"}} />
+       <Carousel.Caption>
+         <h3>First slide label</h3>
+         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+       <button className='btn5 p-3 rounded'> Read More</button>
+  
+       </Carousel.Caption>
+     </Carousel.Item>
+     <Carousel.Item>
+       <img  src={img3 } alt='' text="Second slide" style={{width:"100%" , height:"450px"}} />
+  
+       <Carousel.Caption>
+         <h3>Second slide label</h3>
+         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+         <button className='btn5 p-3 rounded'> Read More</button>
+  
+       </Carousel.Caption>
+     </Carousel.Item>
+     <Carousel.Item>
+       <img  src={img2 } alt='' text="Third slide" style={{width:"100%" , height:"450px"}} />
+  
+       <Carousel.Caption>
+         <h3>Third slide label</h3>
+         <p>
+           Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+         </p>
+       <button className='btn5 p-3 rounded'> Read More</button>
+  
+       </Carousel.Caption>
+     </Carousel.Item>
+   </Carousel>
+
+<div className='d-flex justify-content-around my-5 text-left' style={{gap:"30px" , flexWrap:"wrap"}}>
+<div className='card0    rounded-5 shadow-lg' >
+  <Beenhere style={{fontSize:"45px"}} />
+  <h5 className='my-2'>Services</h5>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit 
+  . Iusto quasi assumenda repellendus ad pariatur, enim earum reiciendis, dolore laborum, magni
+   </p>  
+  </div>
+
+  <div className='card0  rounded-5 shadow-lg' >
+  <Cottage style={{fontSize:"45px"}}  />
+  <h5 className='my-2'>Services</h5>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quasi assumenda repellendus ad pariatur, enim earum reiciendis, dolore laborum, magni
+   </p>    
+  </div>
+
+  <div className='card0  rounded-5 shadow-lg'>
+  <CameraRoll  style={{fontSize:"45px"}}  />
+  <h5 className='my-2'>Services</h5>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quasi assumenda repellendus ad pariatur, enim earum reiciendis, dolore laborum, magni
+   </p>    
+  </div>
+
+  <div className='card0  rounded-5 shadow-lg' >
+  <Balance  style={{fontSize:"45px"}}  />
+  <h5 className='my-2'>Services</h5>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quasi assumenda repellendus ad pariatur, enim earum reiciendis, dolore laborum, magni
+</p>    
+  </div>
+
+
+</div>
+
+<h2 style={{textAlign:"center", marginBottom:"30px" , opacity:".5"}}> Our Vision</h2>
+
+<div className='my-5 align-items-center rounded-5 shadow-lg row' style={{margin:'auto', textAlign:"left" , backgroundColor:"#bcbcb761" , width:"85%"}}>
+      <div className='col-lg-5' >
+          <img src={img2} alt="img" className=' rounded-5 p-3 me-5'style={{width:"100%" ,height:"300px"}}  />
+      </div>
+        <div className='col-lg-5 py-3' style={{textAlign:"justify"}}>
+            <h1>Our Vision</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet quae repellendus ea! Explicabo nemo a quisquam error molestiae, reiciendis tempora veniam molestias voluptatem, magni est numquam quibusdam deserunt sapiente cumque?</p>
+            <button className='btn5 px-3 py-2 mb-3 rounded-4 '>Read More</button>
+ <div className=''>
+               <p> <TaskAlt /> Lorem ipsum dolor sit amet.</p>
+               <p> <TaskAlt /> Lorem ipsum dolor sit amet.</p>
+           <div>
+                   <span className='icon-instagram me-2   icon' />
+                   <span className='icon-linkedin2 me-2   icon' />
+                   <span className='icon-github me-2   icon' />
+                   <span className='icon-youtube me-2   icon' />
+                   <span className='icon-facebook1    icon ' />
+           </div>
+ </div>
+        </div>
+
+    </div>
+
+
+
+
+    <h2 style={{textAlign:"center", marginBottom:"30px" , opacity:".5"}}> Our Mission</h2>
+
+<div className='my-5 align-items-center rounded-5 shadow-lg row' style={{margin:'auto', textAlign:"left" , backgroundColor:"#bcbcb761" , width:"85%"}}>
+      
+        <div className='col-lg-5 py-3' style={{flexGrow:"1"}}>
+            <h1>Our Mission</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet quae <br /> repellendus ea! Explicabo nemo a quisquam error molestiae, reiciendis <br /> tempora veniam molestias voluptatem,  magni est numquam quibusdam <br /> deserunt sapiente cumque?</p>
+            <button className='btn5 px-3 py-2 mb-3 rounded-4 '>Read More</button>
+ <div className=''>
+               <p> <TaskAlt /> Lorem ipsum dolor sit amet.</p>
+               <p> <TaskAlt /> Lorem ipsum dolor sit amet.</p>
+           <div>
+                   <span className='icon-instagram me-2   icon' />
+                   <span className='icon-linkedin2 me-2   icon' />
+                   <span className='icon-github me-2   icon' />
+                   <span className='icon-youtube me-2   icon' />
+                   <span className='icon-facebook1    icon ' />
+           </div>
+ </div>
+        </div>
+        <div className='col-lg-5 '>
+          <img src={img2} alt="img" className=' rounded-5 p-3 me-5'style={{width:"100%" , height:'300px'}} />
+      </div>
+
+    </div>
+    
+
+<div className='count px-5 py-3 m-auto' style={{flexWrap:"wrap" , alignItems:"center" , textAlign:"center"}}>
+<div className='bg-dark p-3 count1 rounded-4' style={{boxShadow:"5px 5px 10px #ffa200"}}>
+  <button className='btn5 py-3 rounded-4 px-5 my-3'>COMPANY FACTS
+
+</button>
+  <p>Lorem ipsum dolor sit amet consectetur <br /> adipisicing elit. Numquam, accusamus.</p>
+</div>
+
+<div className='d-flex p-5 'style={{gap:"30px" , fontSize:"30px" , flexWrap:"wrap" }}>
+  <div className='bg-light  count1 text-dark rounded-5 px-5 py-3'style={{flexDirection:"column" , textAlign:'center'}}>
+    <Beenhere  style={{fontSize:"50px" , display:"block"}}/>
+    <CountUp className='' start={0} end={100} delay={2} duration={20} />
+    
+  </div>
+  
+  <div className='bg-light count1 text-dark rounded-5 px-5 py-3'style={{flexDirection:"column" , textAlign:'center'}}>
+    <Engineering  style={{fontSize:"50px" , display:"block"}}/>
+    <CountUp className='' start={0} end={200} delay={2} duration={20} />
+    
+  </div>
+  <div className='bg-light  count1 text-dark rounded-5 px-5 py-3'style={{flexDirection:"column" , textAlign:'center'}}>
+    <LaptopChromebook  style={{fontSize:"50px" , display:"block"}}/>
+    <CountUp className='' start={0} end={120} delay={2} duration={20} />
+    
+  </div>
+  <div className='bg-light  count1 text-dark rounded-5 px-5 py-3'style={{flexDirection:"column" , textAlign:'center'}}>
+    <LocalPhone  style={{fontSize:"50px" , display:"block"}}/>
+    <CountUp className='' start={0} end={150} delay={2} duration={20} />
+    
+  </div>
+
+
+
+</div>
+
+
+
+
+</div>
+
+
+
+
+
+ </div>
+
+
+);
 }
+
+  
+
 
 export default About
--- src/Pages/4-Products/Products.jsx
+++ src/Pages/4-Products/Products.jsx
@@ -1,9 +1,265 @@
 import React from 'react'
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import "./products.css"
+// import Rating from '@mui/material/Rating';
+import TextField from '@mui/material/TextField';
+import Autocomplete, { createFilterOptions } from '@mui/material/Autocomplete';
+
+
+import img7 from '../../components/images/cam3.jpg'
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
+
 
 const Products = () => {
+  const top100Films = [
+    { title: 'pro1', year: 1994 },
+    { title: 'pro2', year: 1972 },
+    { title: 'pro3', year: 1974 },
+    { title: 'pro4', year: 2008 },
+    { title: 'pro5', year: 1957 },
+    { title: "pro6", year: 1993 },
+    { title: 'pro7', year: 1994 },
+    {
+      title: 'pro8',
+      year: 2003,
+    },
+  ]
+
+  const filterOptions = createFilterOptions({
+    matchFrom: 'start',
+    stringify: (option) => option.title,
+  });
+
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <div style={{display:"flex" , justifyContent:"center" , alignItems:"center" , minHeight:"100vh"}}>Products</div>
-  )
+
+
+   <div className='my-5' style={{minHeight:"100vh"}}>
+   <div className='d-flex align-items-center 'style={{flexWrap:"wrap"}}>
+      <div className=' ms-2' style={{flexGrow:"1"}}>
+         <h3 className=''>Categories</h3>
+         <input type="search" className=' rounded-5 ' placeholder='Search...' style={{outline:"none" , border:"none" ,backgroundColor:"#d2cccc"}} />
+        
+      </div>
+
+      <Autocomplete className='pe-5 my-2 ms-2'
+      id="filter-demo"
+      options={top100Films}
+      getOptionLabel={(option) => option.title}
+      filterOptions={filterOptions}
+      sx={{ width: 300 }}
+      renderInput={(params) => <TextField {...params} label="Custom filter" />}
+    />
+
+    
+
+
+   </div>
+     <Box 
+       sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: 224 }}
+     >
+       <Tabs
+         orientation="vertical"
+         variant="scrollable"
+         value={value}
+         onChange={handleChange}
+         aria-label="Vertical tabs example"
+         sx={{ borderRight: 1, borderColor: 'divider' , minWidth:"100px" }}
+       >
+         <Tab label="Item One" {...a11yProps(0)} />
+         <Tab label="Item Two" {...a11yProps(1)} />
+         <Tab label="Item Three" {...a11yProps(2)} />
+         <Tab label="Item Four" {...a11yProps(3)} />
+         <Tab label="Item Five" {...a11yProps(4)} />
+         <Tab label="Item Six" {...a11yProps(5)} />
+         <Tab label="Item Seven" {...a11yProps(6)} />
+       </Tabs>
+       <TabPanel value={value} index={0} >
+<div className='d-flex' style={{gap:"30px" , flexWrap:"wrap"}}>
+  <div className='card  cd5 rounded-5 shadow-lg' style={{ display:"flex" ,justifyContent:"center" ,alignItems:"center" }}>
+  <img src={img7} alt='' width={200} className='rounded-5 p-2' />
+  
+        {/* <Rating name="read-only" value={5} readOnly className='my-2' /> */}
+  <h5>Product 1</h5>
+  <p> <button className='btn5 py-1 px-3 rounded-3'>Request now</button></p>
+  
+  </div>
+
+  <div className='card  cd5 rounded-5 shadow-lg' style={{ display:"flex" ,justifyContent:"center" ,alignItems:"center" }}>
+  <img src={img7} alt='' width={200} className='rounded-5 p-2' />
+  
+        {/* <Rating name="read-only" value={5} readOnly className='my-2' /> */}
+  <h5>Product 1</h5>
+  <p> <button className='btn5 py-1 px-3 rounded-3'>Request now</button></p>
+  
+  </div>
+
+  <div className='card  cd5 rounded-5 shadow-lg' style={{ display:"flex" ,justifyContent:"center" ,alignItems:"center" }}>
+  <img src={img7} alt='' width={200} className='rounded-5 p-2' />
+  
+        {/* <Rating name="read-only" value={5} readOnly className='my-2' /> */}
+  <h5>Product 1</h5>
+  <p> <button className='btn5 py-1 px-3 rounded-3'>Request now</button></p>
+  
+  </div>
+
+  <div className='card  cd5 rounded-5 shadow-lg' style={{ display:"flex" ,justifyContent:"center" ,alignItems:"center" }}>
+  <img src={img7} alt='' width={200} className='rounded-5 p-2' />
+  
+        {/* <Rating name="read-only" value={5} readOnly className='my-2' /> */}
+  <h5>Product 1</h5>
+  <p> <button className='btn5 py-1 px-3 rounded-3'>Request now</button></p>
+  
+  </div>
+
+  <div className='card  cd5 rounded-5 shadow-lg' style={{ display:"flex" ,justifyContent:"center" ,alignItems:"center" }}>
+  <img src={img7} alt='' width={200} className='rounded-5 p-2' />
+  
+        {/* <Rating name="read-only" value={5} readOnly className='my-2' /> */}
+  <h5>Product 1</h5>
+  <p> <button className='btn5 py-1 px-3 rounded-3'>Request now</button></p>
+  
+  </div>
+
+
+  <div className='card  cd5 rounded-5 shadow-lg' style={{ display:"flex" ,justifyContent:"center" ,alignItems:"center" }}>
+  <img src={img7} alt='' width={200} className='rounded-5 p-2' />
+  
+        {/* <Rating name="read-only" value={5} readOnly className='my-2' /> */}
+  <h5>Product 1</h5>
+  <p> <button className='btn5 py-1 px-3 rounded-3'>Request now</button></p>
+  
+  </div>
+
+
+
+</div>
+
+       </TabPanel>
+       <TabPanel value={value} index={1}>
+       
+       
+       
+<div className='d-flex' style={{gap:"30px" , flexWrap:"wrap"}}>
+  <div className='card  cd5 rounded-5 shadow-lg' style={{ display:"flex" ,justifyContent:"center" ,alignItems:"center" }}>
+  <img src={img7} alt='' width={200} className='rounded-5 p-2' />
+  
+        {/* <Rating name="read-only" value={5} readOnly className='my-2' /> */}
+  <h5>Product 1</h5>
+  <p> <button className='btn5 py-1 px-3 rounded-3'>Request now</button></p>
+  
+  </div>
+
+  <div className='card  cd5 rounded-5 shadow-lg' style={{ display:"flex" ,justifyContent:"center" ,alignItems:"center" }}>
+  <img src={img7} alt='' width={200} className='rounded-5 p-2' />
+  
+        {/* <Rating name="read-only" value={5} readOnly className='my-2' /> */}
+  <h5>Product 1</h5>
+  <p> <button className='btn5 py-1 px-3 rounded-3'>Request now</button></p>
+  
+  </div>
+
+  <div className='card  cd5 rounded-5 shadow-lg' style={{ display:"flex" ,justifyContent:"center" ,alignItems:"center" }}>
+  <img src={img7} alt='' width={200} className='rounded-5 p-2' />
+  
+        {/* <Rating name="read-only" value={5} readOnly className='my-2' /> */}
+  <h5>Product 1</h5>
+  <p> <button className='btn5 py-1 px-3 rounded-3'>Request now</button></p>
+  
+  </div>
+
+
+
+</div>
+
+
+       </TabPanel>
+       <TabPanel value={value} index={2}>
+        
+        
+        
+<div className='d-flex' style={{gap:"30px" , flexWrap:"wrap"}}>
+  <div className='card  cd5 rounded-5 shadow-lg' style={{ display:"flex" ,justifyContent:"center" ,alignItems:"center" }}>
+  <img src={img7} alt='' width={200} className='rounded-5 p-2' />
+  
+        {/* <Rating name="read-only" value={5} readOnly className='my-2' /> */}
+  <h5>Product 1</h5>
+  <p> <button className='btn5 py-1 px-3 rounded-3'>Request now</button></p>
+  
+  </div>
+
+  <div className='card  cd5 rounded-5 shadow-lg' style={{ display:"flex" ,justifyContent:"center" ,alignItems:"center" }}>
+  <img src={img7} alt='' width={200} className='rounded-5 p-2' />
+  
+        {/* <Rating name="read-only" value={5} readOnly className='my-2' /> */}
+  <h5>Product 1</h5>
+  <p> <button className='btn5 py-1 px-3 rounded-3'>Request now</button></p>
+  
+  </div>
+
+
+
+
+
+</div>
+       </TabPanel>
+       <TabPanel value={value} index={3}>
+         Item Four
+       </TabPanel>
+       <TabPanel value={value} index={4}>
+         Item Five
+       </TabPanel>
+       <TabPanel value={value} index={5}>
+         Item Six
+       </TabPanel>
+       <TabPanel value={value} index={6}>
+         Item Seven
+       </TabPanel>
+     </Box>
+   </div>
+
+
+    )
 }
 
 export default Products
